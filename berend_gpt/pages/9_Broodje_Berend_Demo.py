41cda0b4a94515a5bcfc47e8c6c05a662fa68fa0
Ed de Feber <84625097+EdF2021@users.noreply.github.com>
2023-10-18 19:13:44 +0200

    Update 9_Broodje_Berend_Demo.py

--- berend_gpt/pages/9_Broodje_Berend_Demo.py
+++ berend_gpt/pages/9_Broodje_Berend_Demo.py
@@ -52,7 +52,7 @@ uploaded_file = st.file_uploader(
 )
 
 
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+# openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
@@ -78,26 +78,25 @@ if prompt := st.chat_input("Hoe gaat het?"):
     st.session_state.messages.append(
         {
             "role": "user",
-            "content": prompt,
+            "content": prompt
         }
     )
     with st.chat_message("user"):
         st.markdown(prompt)
         
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
-            for response in openai.ChatCompletion.create(
-                model=st.session_state["openai_model"],
-                messages=[
-                {"role": m["role"], "content": m["content"]}
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+            {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
-                ],
+            ],
                 stream=True,
-            ):
-                full_response += response.choices[0].delta.get("content", "")
-                message_placeholder.markdown(full_response + "▌")
-
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
         st.session_state.messages.append({"role": "assistant", "content": full_response})
         response == " "
