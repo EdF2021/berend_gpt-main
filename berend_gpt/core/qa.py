24d5ce2e352456d34afb216d8aecf4d908d6f83a
Ed. de Feber <84625097+EdF2021@users.noreply.github.com>
2024-01-04 13:09:19 +0100

    Update qa.py
    Signed-off-by: Ed. de Feber <84625097+EdF2021@users.noreply.github.com>

--- berend_gpt/core/qa.py
+++ berend_gpt/core/qa.py
@@ -1,8 +1,8 @@
 from typing import List
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
-from berend_gpt.core.prompts import STUFF_PROMPT
+from core.prompts import STUFF_PROMPT
 from langchain.docstore.document import Document
-from berend_gpt.core.embedding import FolderIndex
+from core.embedding import FolderIndex
 from pydantic import BaseModel
 from langchain.chat_models.base import BaseChatModel
 
